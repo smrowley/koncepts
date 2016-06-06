8fad308926e0607efc7fc93f686d6dc7cbc32f5a
Shane Rowley <srowley@redhat.com>
2016-06-06 13:47:45 +1000

    squashed commits

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", message="Hello Flask!");   
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
