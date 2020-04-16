33a5f550f8c11e178ea1cb35c76a52c7979d7bde
Shane Rowley <srowley@redhat.com>
2020-04-15 23:38:31 -0400

    added hostname

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
+hostname = os.popen("cat /etc/hostname").read().strip()
+
 
 @application.route("/")
 def index():
-    return render_template("index.html", message="Hello Flask!")
+    return render_template("index.html", hostname=hostname)
+
+
+@application.route("/plain")
+def plain():
+    return hostname
 
 
 if __name__ == "__main__":
