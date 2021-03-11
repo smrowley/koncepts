0580ee249c3656e5a2c00038211c5a728e15ff8e
Shane Rowley <srowley@redhat.com>
2021-03-10 21:50:01 -0500

    cast port as int for gunicorn metrics

--- src/gunicorn.config.py
+++ src/gunicorn.config.py
@@ -2,7 +2,7 @@ from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 import os
 
 def when_ready(server):
-    GunicornPrometheusMetrics.start_http_server_when_ready(os.environ["METRICS_PORT"])
+    GunicornPrometheusMetrics.start_http_server_when_ready(int(os.environ["METRICS_PORT"]))
 
 def child_exit(server, worker):
     GunicornPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
